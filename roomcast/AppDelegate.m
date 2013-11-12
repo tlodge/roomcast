10e36771005d3ced7b1dcebca3506d734b6ef46e
tlodge <tlodge@gmail.com>
2013-11-12 18:11:59 +0000

    core data stuff

--- roomcast/AppDelegate.m
+++ roomcast/AppDelegate.m
@@ -8,10 +8,63 @@
 
 #import "AppDelegate.h"
 
+
 @implementation AppDelegate
+
+@synthesize managedObjectContext = __managedObjectContext;
+@synthesize managedObjectModel = __managedObjectModel;
+@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+   //self.client = [[SMClient alloc] initWithAPIVersion:@"0" publicKey:@"25697993-b276-4e20-b921-bf80d10ff543"];
+  
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
+    NSManagedObject *conversation = [NSEntityDescription
+                                      insertNewObjectForEntityForName:@"Conversation"
+                                      inManagedObjectContext:context];
+    
+    [conversation setValue:@"1A" forKey:@"initiator"];
+    [conversation setValue:@"our bins" forKey:@"teaser"];
+    [conversation setValue:[NSDate date] forKey:@"started"];
+    
+    NSManagedObject *message1 = [NSEntityDescription
+                                      insertNewObjectForEntityForName:@"Message"
+                                      inManagedObjectContext:context];
+    
+    [message1 setValue:@"1A" forKey:@"from"];
+    [message1 setValue:@"our bins smell pretty bad at the moment.  why?" forKey:@"body"];
+    [message1 setValue:[NSDate date] forKey:@"sent"];
+    [message1 setValue:conversation forKey:@"conversation"];
+    
+    NSManagedObject *message2 = [NSEntityDescription
+                                 insertNewObjectForEntityForName:@"Message"
+                                 inManagedObjectContext:context];
+    
+    [message2 setValue:@"1B" forKey:@"from"];
+    [message2 setValue:@"agreed - they're absolutely awful!" forKey:@"body"];
+    [message2 setValue:[NSDate date] forKey:@"sent"];
+    [message2 setValue:conversation forKey:@"conversation"];
+    
+    NSError *error;
+    if (![context save:&error]){
+        NSLog(@"whoops! couldn't save %@", [error localizedDescription]);
+    }
+    
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Conversation" inManagedObjectContext:context];
+    [fetchRequest setEntity:entity];
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+    
+    for (NSManagedObject *info in fetchedObjects){
+        NSLog(@"Init %@", [info valueForKey:@"initiator"]);
+        NSManagedObject *messages = [info valueForKey:@"messages"];
+        NSLog(@"teaser %@", [info valueForKey:@"teaser"]);
+    }
+    
+    
     // Override point for customization after application launch.
     return YES;
 }
@@ -42,5 +95,118 @@
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil) {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }
+}
+
+#pragma mark - Core Data stack
+
+// Returns the managed object context for the application.
+// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (__managedObjectContext != nil) {
+        return __managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        __managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [__managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return __managedObjectContext;
+}
+
+// Returns the managed object model for the application.
+// If the model doesn't already exist, it is created from the application's model.
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (__managedObjectModel != nil) {
+        return __managedObjectModel;
+    }
+    NSLog(@"ok am heer.......");
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Roomcast" withExtension:@"momd"];
+    NSLog(@"*** the url is %@", modelURL);
+    __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return __managedObjectModel;
+}
+
+// Returns the persistent store coordinator for the application.
+// If the coordinator doesn't already exist, it is created and the application's store added to it.
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    if (__persistentStoreCoordinator != nil) {
+        return __persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Roomcast.sqlite"];
+    
+    NSLog(@"url is %@", storeURL);
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:[storeURL path]]) {
+        NSLog(@"file does not yet exist!");
+        NSURL *preloadURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Roomcast" ofType:@"sqlite"]];
+        
+        NSLog(@"pre load url is %@", preloadURL);
+        NSError* err = nil;
+        
+        if (![[NSFileManager defaultManager] copyItemAtURL:preloadURL toURL:storeURL error:&err]) {
+            NSLog(@"Oops, could copy preloaded data");
+        }
+        NSLog(@"ok so far so good...");
+    }
+    
+    NSError *error = nil;
+    __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+         
+         Typical reasons for an error here include:
+         * The persistent store is not accessible;
+         * The schema for the persistent store is incompatible with current managed object model.
+         Check the error message to determine what the actual problem was.
+         
+         
+         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
+         
+         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
+         * Simply deleting the existing store:
+         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
+         
+         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
+         [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+         
+         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+         
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return __persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+// Returns the URL to the application's Documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
+
 
 @end
